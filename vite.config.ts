0a3e0a3a5bb220a63ca5cba1b8bf3c5957a3ace8
asiyashaikh02 <119112698+asiyashaikh02@users.noreply.github.com>
2025-09-02 13:31:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+export default defineConfig({
+  plugins: [react()],
+  base: '/',   // ✅ For custom domain, always "/"
+})
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/dr_siddhi_fuke/',   // 👈 ye line zaroori hai
-})
+//   base: '/dr_siddhi_fuke/',   // 👈 ye line zaroori hai
+// })
