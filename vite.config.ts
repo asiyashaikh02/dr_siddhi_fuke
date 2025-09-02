0575a6ee4af7a6d88f78ba5a6b9c73d578b713df
asiyashaikh02 <shkasiya02@gmail.com>
2025-09-02 13:56:26 +0530

    Fix deploy workflow and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/', // ✅ custom domain ke liye sahi
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/dr_siddhi_fuke/',   // 👈 ye line zaroori hai
-// })
