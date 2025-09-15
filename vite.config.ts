08d22016778d21de0657b2ba5410d0841b08e7bd
asiyashaikh02 <shkasiya02@gmail.com>
2025-09-15 21:29:13 +0530

    Use custom domain base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dr_siddhi_fuke/', // ✅ correct path for GitHub Pages
+  base: '/', // ✅ correct for custom domain
 })
